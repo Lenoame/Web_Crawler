839f34902f2e9064fb3d37ab340074f9a1a891a6
Lenoame <seleno1104@gmail.com>
2021-12-02 12:51:08 +0900

    자동로그인 기능 만들기

--- INSTAGRAM/app.py
+++ INSTAGRAM/app.py
@@ -5,9 +5,16 @@ import time
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://instagram.com')
 
+# 인스타그램 자동 로그인
+e = driver.find_element_by_css_selector('input[name="username"]')
+e.send_keys('내아이디')
+e.send_keys(Keys.ENTER)
+
+
+
 # 2초간 코드 실행을 멈춰주세요
-time.sleep(2)
-# 여기 마침표는 클래스라는 뜻
-e = driver.find_element_by_css_selector('.b_nGN').text
-print(e)
+# time.sleep(2)
+# # 여기 마침표는 클래스라는 뜻
+# e = driver.find_element_by_css_selector('.b_nGN').text
+# print(e)
 
