b48bebaf1f0bb08dd6f7fd23e497f968bdba5427
Lenoame <seleno1104@gmail.com>
2021-12-03 22:39:13 +0900

    페이지 이동 & 이미지 수집

--- INSTAGRAM/app.py
+++ INSTAGRAM/app.py
@@ -6,15 +6,24 @@ driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://instagram.com')
 
 # 인스타그램 자동 로그인
+time.sleep(3)
 e = driver.find_element_by_css_selector('input[name="username"]')
-e.send_keys('내아이디')
-e.send_keys(Keys.ENTER)
+e.send_keys('leno1010')
+e = driver.find_element_by_css_selector('input[name="password"]')
+e.send_keys('asdf1234*')
+e.send_keys(Keys_ENTER)
 
+# 페이지 이동
+time.sleep(2)
+driver.get('https://instagram.com/explore/tags/%EC%%82%AC%EA%B3%BC/')
 
+# 첫째사진누름
+# 이 요소가 없으면 최대 10초간 기다려줘
+driver.implicitly_wait(10)
+e = driver.find_element_by_css_selector('._9AhH0').click()
+# 클래스 명이 여러 개 있으면 멘 위에 하나만 찾아줌
+# 다른 것 찾고 싶으면 element -> elements로 바꿔주기 -> 찾아서 리스트에 담아준다
 
-# 2초간 코드 실행을 멈춰주세요
-# time.sleep(2)
-# # 여기 마침표는 클래스라는 뜻
-# e = driver.find_element_by_css_selector('.b_nGN').text
-# print(e)
-
+# 사진 저장
+img = driver.find_element_by_css_selector('.FFVAD').get_attribute('src')
+urllib.request.urlretrieve(img, '1.jpg')
