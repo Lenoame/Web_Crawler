7939609a083ab48a294c7ac9c550632ed0fc9e43
Lenoame <seleno1104@gmail.com>
2021-12-04 23:42:29 +0900

    여러 사진을 연속해서 저장하기 자동화

--- INSTAGRAM/app.py
+++ INSTAGRAM/app.py
@@ -21,9 +21,18 @@ driver.get('https://instagram.com/explore/tags/%EC%%82%AC%EA%B3%BC/')
 # 이 요소가 없으면 최대 10초간 기다려줘
 driver.implicitly_wait(10)
 e = driver.find_element_by_css_selector('._9AhH0').click()
-# 클래스 명이 여러 개 있으면 멘 위에 하나만 찾아줌
-# 다른 것 찾고 싶으면 element -> elements로 바꿔주기 -> 찾아서 리스트에 담아준다
+# # 클래스 명이 여러 개 있으면 멘 위에 하나만 찾아줌
+# # 다른 것 찾고 싶으면 element -> elements로 바꿔주기 -> 찾아서 리스트에 담아준다
 
-# 사진 저장
-img = driver.find_element_by_css_selector('.FFVAD').get_attribute('src')
-urllib.request.urlretrieve(img, '1.jpg')
+# # 사진 저장
+# img = driver.find_element_by_css_selector('.FFVAD').get_attribute('src')
+# urllib.request.urlretrieve(img, '1.jpg')
+
+for i in range(50):
+	# 사진저장 formatting 문법 사용
+	img = driver.find_element_by_css_selector('_97aPb .FFVAD').get_attribute('src')
+	urllib.request.urlretrieve(img, f'{i}.jpg')
+	
+	# 다음
+	e = driver.find_element_by_css_selector('._65Bje').click()
+	driver.execute_script('arguments[0].click();'. e)
